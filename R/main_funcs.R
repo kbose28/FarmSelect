d9492197a8c4f934940b55bdbbf6f6b7bde6a361
KOUSHIKI BOSE <koushiki.bose@gmail.com>
2018-01-29 10:33:06 -0500

    run p simulation

--- R/main_funcs.R
+++ R/main_funcs.R
@@ -17,12 +17,12 @@ NULL
 #' @param Y a size n outcome vector.
 #' @param X an n x p covariate matrix with each row being a sample. Must have same number of rows as the size of \code{Y}.
 #' @param loss an \emph{optional} character string specifying the loss function to be minimized. Must be one of "mcp" (default), "scad" or "lasso". You can just specify the initial letter.
-#' @param verbose an \emph{optional} boolean determining whether or not to print output to the console. Default is TRUE.
-#' @param robust an \emph{optional} boolean, specifying whether or not to use robust estimators for mean and variance. Default is FALSE.
-#' @param lin.reg an \emph{optional} boolean, specifying whether or not to assume that we have a linear regression model (TRUE) or a logit model (FALSE) structure. Default is TRUE.
+#' @param verbose a boolean determining whether or not to print output to the console. Default is TRUE.
+#' @param robust a boolean, specifying whether or not to use robust estimators for mean and variance. Default is FALSE.
+#' @param lin.reg a boolean, specifying whether or not to assume that we have a linear regression model (TRUE) or a logit model (FALSE) structure. Default is TRUE.
 #' @param K.factors a \emph{optional} number of factors to be estimated. Otherwise estimated internally.
-#' @param max.iter The maximum number of passes over the data for each value of regularization parameter for model fitting. Default \code{10^3}.
-#' @param nfolds The number of cross-validation folds. Default is 10.
+#' @param max.iter Maximum number of iterations for across the regularization path. Default is 10000.
+#' @param nfolds The number of cross-validation folds. Default is ceiling(samplesize/3).
 #' @return A list with the following items
 #' \item{model.size}{the size of the model}
 #' \item{beta.chosen}{the indices of the covariates chosen in the model}
@@ -52,26 +52,26 @@ NULL
 #' output = farm.select(Y,X)
 #'
 #' ##changing the loss function and inputting factors
-#' output = farm.select(Y,X, loss = "scad", K.factors = 10)
+#' output = farm.select(Y,X, loss = "lasso", K.factors = 3)
 #'
 #' ##use a logistic regression model
 #' Prob = 1/(1+exp(-X%*%beta))
-#' Y = rbinom(N, 1, Prob)
-#' output = farm.select(Y,X, lin.reg=FALSE, loss="lasso")
+#' Y = rbinom(n, 1, Prob)
+#' output = farm.select(Y,X, lin.reg=FALSE)
 #'
 #' @references Fan J., Ke Y., Wang K., "Decorrelation of Covariates for High Dimensional Sparse Regression." \url{https://arxiv.org/abs/1612.08490}
 #' @export
-farm.select <- function (Y, X,  loss=c("mcp", "scad", "lasso"), verbose = TRUE, robust = FALSE,  lin.reg = TRUE,K.factors= NULL, max.iter=1000,nfolds = 10){
-  loss = match.arg(loss)
+farm.select <- function (Y, X,  loss=c("mcp", "scad", "lasso"), verbose = TRUE, robust = FALSE,  lin.reg = TRUE,K.factors= NULL,max.iter=10000, nfolds=ceiling(length(Y)/3)){
+  loss=match.arg(loss)
   #error checking
   X = t(X)
   if(NCOL(X)!=NROW(Y)) stop('number of rows in covariate matrix should be size of the outcome vector')
-    output.final = farm.select.adjusted(Y, X,   loss=loss, robust,lin.reg,K.factors, max.iter,nfolds )
+    output.final = farm.select.adjusted(Y, X,   loss=match.arg(loss), robust=robust,lin.reg=lin.reg,K.factors=K.factors, max.iter = max.iter, nfolds = nfolds)
     if(verbose){output.final.call = match.call()
-    cat("\nCall:\n")
+    cat("\n Call:\n")
     print(output.final.call)
     cat("\n Factor Adjusted Robust Model Selection \n")
-    cat(paste("loss function used: ",  loss, "\n",sep = ""))
+    cat(paste("loss function used: ",  match.arg(loss), "\n",sep = ""))
     cat(paste("\np = ", NROW(X),", n = ", NCOL(X),"\n", sep = ""))
     cat(paste("factors found: ", output.final$nfactors, "\n", sep = ""))
     cat("size of model selected:\n")
@@ -83,7 +83,7 @@ farm.select <- function (Y, X,  loss=c("mcp", "scad", "lasso"), verbose = TRUE, 
 ###################################################################################
 ## main function: not for end user
 ###################################################################################
-farm.select.adjusted <- function (Y, X,   loss,robust ,lin.reg,K.factors, max.iter,nfolds ){
+farm.select.adjusted <- function (Y, X,   loss,robust ,lin.reg,K.factors, max.iter, nfolds){
   n = length(Y)
   p = NROW(X)
   if(robust ==TRUE){
@@ -107,13 +107,15 @@ farm.select.adjusted <- function (Y, X,   loss,robust ,lin.reg,K.factors, max.it
   nfactors = output.adjust$nfactors
   nx = NROW(X.res)
   p = NCOL(X.res)
+
   #perform model selection
   if(lin.reg){
-   output.chosen = farm.select.temp ( X.res, Y.res, loss=loss, max.iter,nfolds)
+   output.chosen = farm.select.temp ( X.res, Y.res, loss,max.iter, nfolds)
   }else{
     F_hat =  output.adjust$F_hat
-    output.chosen = farm.select.temp ( X.res, Y.res, loss=loss, max.iter,nfolds ,factors = F_hat )
+    output.chosen = farm.select.temp ( X.res, Y.res, loss,max.iter, nfolds, factors = F_hat)
   }
+
 
   #list all the output
   list(model.size = output.chosen$model_size, beta.chosen = output.chosen$beta_chosen, coef.chosen = output.chosen$coef_chosen, nfactors = nfactors, X.res = X.res)
@@ -125,7 +127,7 @@ farm.adjust<- function(Y , X , robust , lin.reg,K.factors ) {#, robust.cov = FAL
   X = t(X)
   p = NROW(X)
   n = NCOL(X)
-  if(min(n,p)<=4) stop('n and p must be at least 4')
+  if(min(n,p)<=4) stop('\n n and p must be at least 4 \n')
 
   if(any(abs(rowMeans(X))>0.00000001)){
     if(robust ==TRUE){
@@ -153,8 +155,8 @@ farm.adjust<- function(Y , X , robust , lin.reg,K.factors ) {#, robust.cov = FAL
       ratio=append(ratio, values[i+1]/values[i])}
     ratio = ratio[is.finite(ratio)]
     K.factors = which.min(ratio)} else {K.factors}
-  if(K.factors>min(n,p)/2) warning('\n Warning: Number of factors supplied is > min(n,p)/2. May cause numerical inconsistencies. \n')
-  if(K.factors>max(n,p)) stop('\n Number of factors cannot be larger than n or p. \n')
+  if(K.factors>min(n,p)/2) warning('\n Warning: Number of factors supplied is > min(n,p)/2. May cause numerical inconsistencies \n')
+  if(K.factors>max(n,p)) stop('\n Number of factors cannot be larger than n or p \n')
 
   #using K.factors estimate the factors and loadings
   if(lin.reg){
@@ -170,7 +172,7 @@ farm.adjust<- function(Y , X , robust , lin.reg,K.factors ) {#, robust.cov = FAL
   if(is.null(Y)){
   list(X.res = X.res, nfactors = K.factors)
   }else {
-    if(NCOL(X)!=NROW(Y)) stop('number of rows in covariate matrix should be size of the outcome vector')
+    if(NCOL(X)!=NROW(Y)) stop('\n number of rows in covariate matrix should be size of the outcome vector \n')
     if(abs(mean(Y))>0.00000001){
       if(robust ==TRUE){
         Y.mean =mu_robust_F(matrix(Y,1,n), matrix(rep(1,n),n,1))
@@ -193,12 +195,13 @@ farm.adjust<- function(Y , X , robust , lin.reg,K.factors ) {#, robust.cov = FAL
   }
 }
 #model selection: not for end user
-farm.select.temp<- function(X.res, Y.res,loss, max.iter,nfolds, factors = NULL)
+farm.select.temp<- function(X.res, Y.res,loss, max.iter, nfolds ,factors = NULL)
 {
-N = length(Y.res)
+
+  N = length(Y.res)
   if(is.null(factors)){
     if (loss == "scad"){
-      CV_SCAD=cv.ncvreg(X.res, Y.res,penalty="SCAD",seed = 100, nfolds = nfolds, max.iter=max.iter)
+      CV_SCAD=cv.ncvreg(X.res, Y.res,penalty="SCAD",seed = 100, nfolds = nfolds, max.iter  = max.iter)
       beta_SCAD=coef(CV_SCAD, s = "lambda.min", exact=TRUE)
       inds_SCAD=which(beta_SCAD!=0)
       if( length(inds_SCAD)==1){
@@ -211,7 +214,7 @@ N = length(Y.res)
         }
       }
     else if (loss == "lasso"){
-      CV_lasso=cv.ncvreg(X.res, Y.res,penalty="lasso",seed = 100, nfolds = nfolds, max.iter=max.iter)
+      CV_lasso=cv.ncvreg(X.res, Y.res,penalty="lasso", seed = 100,nfolds = cnfolds, max.iter  = max.iter)
       beta_lasso=coef(CV_lasso, s = "lambda.min", exact=TRUE)
       inds_lasso=which(beta_lasso!=0)
       if( length(inds_lasso)==1){
@@ -223,7 +226,7 @@ N = length(Y.res)
           list(beta_chosen= inds_lasso, coef_chosen=coef_chosen,model_size=length(inds_lasso))
           }
       }else{
-        CV_MCP=cv.ncvreg(X.res, Y.res,penalty="MCP",seed=100,nfolds = nfolds, max.iter=max.iter)
+        CV_MCP=cv.ncvreg(X.res, Y.res,penalty="MCP",seed=100,nfolds = nfolds, max.iter  = max.iter)
         beta_MCP=coef(CV_MCP, s = "lambda.min", exact=TRUE)
         inds_MCP=which(beta_MCP!=0)
         if( length(inds_MCP)==1){
@@ -238,7 +241,7 @@ N = length(Y.res)
     }else{
       Kfactors = NCOL(factors)
       if (loss == "scad"){
-        CV_SCAD=cv.ncvreg(cbind(factors,X.res), Y.res,penalty="SCAD",seed = 100, nfolds = nfolds, family = "binomial", max.iter=max.iter)
+        CV_SCAD=cv.ncvreg(cbind(factors,X.res), Y.res,penalty="SCAD",seed = 100, nfolds = nfolds, family = "binomial", max.iter  = max.iter)
         beta_SCAD=coef(CV_SCAD, s = "lambda.min", exact=TRUE)
         beta_SCAD = beta_SCAD[-c(2:(Kfactors+1))]
         inds_SCAD=which(beta_SCAD!=0)
@@ -252,7 +255,7 @@ N = length(Y.res)
           }
         }
       else if (loss == "lasso"){
-        CV_lasso=cv.ncvreg(cbind(factors,X.res), Y.res, penalty="lasso",seed = 100,nfolds = nfolds, family = "binomial", max.iter=max.iter)
+        CV_lasso=cv.ncvreg(cbind(factors,X.res), Y.res,penalty="lasso", seed = 100,nfolds = nfolds, family = "binomial", max.iter  = max.iter)
          beta_lasso=coef(CV_lasso, s = "lambda.min", exact=TRUE)
         beta_lasso = beta_lasso[-c(2:(Kfactors+1))]
         inds_lasso=which(beta_lasso!=0)
@@ -265,7 +268,7 @@ N = length(Y.res)
             list(beta_chosen= inds_lasso, coef_chosen=coef_chosen,model_size=length(inds_lasso) )
             }
         }else {
-          CV_MCP=cv.ncvreg(cbind(factors,X.res), Y.res,penalty="MCP",seed=100,nfolds = nfolds, family = "binomial", max.iter=max.iter)
+          CV_MCP=cv.ncvreg(cbind(factors,X.res), Y.res,penalty="MCP",seed=100,nfolds = nfolds, family = "binomial", max.iter  = max.iter)
           beta_MCP=coef(CV_MCP, s = "lambda.min", exact=TRUE)
           beta_MCP = beta_MCP[-c(2:(Kfactors+1))]
           inds_MCP=which(beta_MCP!=0)
@@ -309,9 +312,8 @@ N = length(Y.res)
 #' U = matrix(rnorm(P*N, 0,1), P,N)
 #' X = Lambda%*%t(F)+U
 #' X = t(X)
-#' output = farm.res(X) #default options
+#' output = farm.res(X)
 #'
-#' output = farm.res(X, K.factors = 1) #specify number of factors
 #' @references Ahn, S. C., and A. R. Horenstein (2013): "Eigenvalue Ratio Test for the Number of Factors," Econometrica, 81 (3), 1203–1227.
 #' @references Fan J., Ke Y., Wang K., "Decorrelation of Covariates for High Dimensional Sparse Regression." \url{https://arxiv.org/abs/1612.08490}
 #' @export
@@ -319,7 +321,7 @@ farm.res<- function(X ,K.factors = NULL, robust = FALSE) {#, robust.cov = FALSE)
   X = t(X)
   p = NROW(X)
   n = NCOL(X)
-  if(min(n,p)<=4) stop('n and p must be at least 4')
+  if(min(n,p)<=4) stop('\n n and p must be at least 4 \n')
 
   if(any(abs(rowMeans(X))>0.00000001)){
     if(robust ==TRUE){
@@ -347,14 +349,14 @@ farm.res<- function(X ,K.factors = NULL, robust = FALSE) {#, robust.cov = FALSE)
       ratio=append(ratio, values[i+1]/values[i])}
     ratio = ratio[is.finite(ratio)]
     K.factors = which.min(ratio)} else {K.factors}
-  if(K.factors>min(n,p)/2) warning('\n Warning: Number of factors supplied is > min(n,p)/2. May cause numerical inconsistencies. \n')
-  if(K.factors>max(n,p)) stop('\n Number of factors cannot be larger than n or p. \n')
+  if(K.factors>min(n,p)/2) warning('\n Warning: Number of factors supplied is > min(n,p)/2. May cause numerical inconsistencies \n')
+  if(K.factors>max(n,p)) stop('\n Number of factors cannot be larger than n or p \n')
 
   #using K.factors estimate the factors and loadings
   Lambda_hat = Find_lambda_class(Sigma = matrix(covx,p,p), (X), n, p,  K.factors)
   F_hat = Find_factors_class(Lambda_hat, (X), n, p,  K.factors)
   X.res = Find_X_star_class(F_hat,Lambda_hat, X )
 
-  list(X.res = X.res, nfactors = K.factors, factod = F_hat, loadings  = Lambda_hat)
+  list(X.res = X.res, nfactors = K.factors, factors = F_hat, loadings  = Lambda_hat)
 
 }
